734aec643d0b958ec7e1973e8d11d5230296ede3
Karol <szrederkarol1@gmail.com>
2023-05-18 22:12:25 +0200

    Page narrowing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Navigation from "./first_view/Navigation.jsx";
 import Skills from "./skills_view/skills.jsx";
+import "./App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
