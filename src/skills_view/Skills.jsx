b427230726fe6227bc07e0f60205d1154aa3135b
Karol <szrederkarol1@gmail.com>
2023-05-18 11:37:52 +0200

    Add styles in section Skills

--- src/skills_view/Skills.jsx
+++ src/skills_view/Skills.jsx
@@ -8,14 +8,20 @@ function Skills() {
         <div className="skills_container">
           <h2 className="tittle_skills">HOW I CAN SERVE YOU</h2>
           <div className="first_skill">
-            <span>
-              <i class="fa-solid fa-laptop-code fa-4x"></i>
-            </span>
-            <h3 className="tittle_first_skill">Frontend Development</h3>
-            <p>
-              I have fluent knowledge of HTML, CSS and good knowledge of
-              JavaScript. I have knowledge about the React framework.
-            </p>
+            <div className="mały_div">
+              <div className="icon_and_title_first_skill">
+                <span style={{color: "yellow", fontWeight: "100"}}>
+                  <i className="fa-solid fa-laptop-code fa-4x fa-flip"></i>
+                </span>
+                <h3 className="tittle_first_skill">
+                  Frontend <br></br>Development
+                </h3>
+              </div>
+              <p className="paragraph_first_skill">
+                I have fluent knowledge of HTML, CSS and good knowledge of
+                JavaScript. I have knowledge about the React framework.
+              </p>
+            </div>
           </div>
         </div>
       </section>
