eccf58b04125cc8e58687b646cce7081be326d7f
Karol <szrederkarol1@gmail.com>
2023-05-31 00:10:57 +0200

    Finished SignIn and SignUp.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,24 +1,29 @@
 import supabase from "../services/supabase";
-const SignIn = () => {
-  const handleSignIn = asunc(e) =>
-  {
-    e.preventDefault();
-    const {email, password} = e.target.elements;
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-    let { data, error } = await supabase.auth.signInWithPassword({
+const SignIn = () => {
+  const [authError, setauthError] = useState(null);
+  const navigation = useNavigate();
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const { email, password } = e.target.elements;
+
+    let { error } = await supabase.auth.signInWithPassword({
       email: email.value,
-      password: password.value
+      password: password.value,
     });
-    if(!error) {
-     console.log(data);
-     return;
+    if (!error) {
+      navigation("/");
+      return;
     }
-    console.error(error);
-  }
+    setauthError(error.message);
+  };
   return (
     <>
       <h1>Sign In</h1>
-      <form onSubmit={handleSignIp}>
+      {authError && <div style={{ color: "red" }}>{authError}</div>}
+      <form onSubmit={handleSignIn}>
         <input id="email" placeholder="Enter Your email" />
         <input id="password" type="password" placeholder="Enter the password" />
         <button>Log in</button>
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://luthfoiccdjrylrvuair.supabase.co",
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1dGhmb2ljY2RqcnlscnZ1YWlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0Nzc2NzcsImV4cCI6MjAwMTA1MzY3N30.s_rIQ9oMZbGIi0DBfhGIrGyF209rTBbb6Wt8A9s6tV4'
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1dGhmb2ljY2RqcnlscnZ1YWlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0Nzc2NzcsImV4cCI6MjAwMTA1MzY3N30.s_rIQ9oMZbGIi0DBfhGIrGyF209rTBbb6Wt8A9s6tV4"
 );
 export default supabase;
