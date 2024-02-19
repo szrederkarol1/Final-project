0c2eeb335d297fa291f84bad8c20ddae319a9084
Karol Szreder <szrederkarol1@gmail.com>
2024-02-19 21:13:15 +0100

    Section location complete

--- src/components/my_location/Location.jsx
+++ src/components/my_location/Location.jsx
@@ -3,14 +3,16 @@ import "./location.scss";
 const Location = () => (
   <>
     <a name="location"></a>
-    <h2 className="location_tittle" style={{ textAlign: "center" }}>
-      WHERE WILL YOU FIND ME
-    </h2>
-    <iframe
-      src="https://maps.google.com/maps?q=54.534278, 17.743194&z=15&output=embed"
-      width="100%"
-      height="600px"
-    ></iframe>
+    <div className="container-location">
+      <h2 className="location_tittle" style={{ textAlign: "center" }}>
+        WHERE WILL YOU FIND ME
+      </h2>
+      <iframe
+        src="https://maps.google.com/maps?q=54.534278, 17.743194&z=15&output=embed"
+        width="100%"
+        height="600px"
+      ></iframe>
+    </div>
   </>
 );
 
